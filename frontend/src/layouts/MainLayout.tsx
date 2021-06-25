fdc1af6cc263ff39590d28c7146fa36f16fbe2f5
dariusclop <clop_darius@yahoo.com>
2021-06-25 15:36:07 +0300

    Add routes and atom design

---
+++ frontend/src/layouts/MainLayout.tsx
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react';
+import { Header } from '../components';
+
+interface IMainLayout {
+    children?: React.ReactNode;
+}
+
+const MainLayout: React.FC<{}> = ({children}: IMainLayout) => {
+  return (
+    <Fragment>
+        <Header />
+        <div style={{marginTop: '24px'}}>
+            {children}
+        </div>
+    </Fragment>
+  )
+}
+
+export default MainLayout;
